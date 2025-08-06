709f59d43f36f378c6b65a0e00e53701c9814ab8
Sude Akdil <sakdil@k1m38s01.42istanbul.com.tr>
2025-08-06 15:59:08 +0300

    updated

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -3,27 +3,28 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strncpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: segunes <segunes@student.42istanbul.com    +#+  +:+       +#+        */
+/*   By: sakdil <sakdil@student.42istanbul.com.t    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 22:16:07 by segunes           #+#    #+#             */
-/*   Updated: 2025/05/10 22:22:51 by segunes          ###   ########.fr       */
+/*   Updated: 2025/08/06 15:58:03 by sakdil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strncpy(char *dest, const char *src, size_t n) 
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-    size_t i;
-    
-    i = 0;
-    while (i < n && src[i]) {
-        dest[i] = src[i];
-        i++;
-    }
-    while (i < n) {
-        dest[i] = '\0';
-        i++;
-    }
-    return (dest);
+	size_t	i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n) {
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -35,7 +35,7 @@ char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strrchr(const char *s, int c);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
-char *ft_strncpy(char *dest, const char *src, size_t n);
+char	*ft_strncpy(char *dest, const char *src, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 void	ft_putchar_fd(char c, int fd);
 void	*ft_memmove(void *dst, const void *src, size_t n);
